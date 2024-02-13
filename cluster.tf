9a587538e86cbd6bd329ace612abe10b68b2f8e8
vinod sjml <vinsjml@gmail.com>
2024-02-13 17:19:00 +0530

    push

--- cluster.tf
+++ cluster.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "mysql" {
-  allocated_storage    = 1
+  allocated_storage    = 10
   identifier          = "roboshop-${var.ENV}-mysql"
   engine               = "mysql"
   engine_version       = "5.7"
