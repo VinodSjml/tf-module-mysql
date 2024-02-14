a78d7ce4cc5f1971e1d3123b807ff7c22377f557
vinod sjml <vinsjml@gmail.com>
2024-02-14 11:08:47 +0530

    push

--- schema.tf
+++ schema.tf
@@ -7,7 +7,7 @@ depends_on = [aws_db_instance.mysql]
         curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
         unzip -o /tmp/mysql.zip
         cd mysql-main
-        mysql  -h ${aws_db_instance.mysql.address} -uadmin1 -proboshop1 < shipping.sql
+        mysql -h ${aws_db_instance.mysql.address} -uadmin1 -proboshop1 < shipping.sql
     EOF
   }
 }
